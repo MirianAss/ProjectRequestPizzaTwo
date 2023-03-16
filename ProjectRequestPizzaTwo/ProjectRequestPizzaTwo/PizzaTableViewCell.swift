1d243978cc531bee0a03012348036ac52897bc8b
Mirian Santana <miriansantana93@gmail.com>
2023-03-16 17:17:20 -0300

    Added dataSource, screen Pizza and screen PizzaTableViewCell with xib

---
+++ ProjectRequestPizzaTwo/ProjectRequestPizzaTwo/PizzaTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PizzaTableViewCell.swift
+//  ProjectRequestPizzaTwo
+//
+//  Created by Mirian Santana on 16/03/23.
+//
+
+import UIKit
+import SDWebImage
+
+class PizzaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var ImagePizza: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func setupCell(pizza: PizzaElement?) {
+        lblName.text = pizza?.name ?? ""
+        let urlImage = URL(string: pizza?.imageURL ?? "")
+        ImagePizza.sd_setImage(with: urlImage)
+    }
+}
---
+++ ProjectRequestPizzaTwo/ProjectRequestPizzaTwo/TableViewViewController.swift
@@ -0,0 +1,50 @@
+//
+//  TableViewViewController.swift
+//  ProjectRequestPizzaTwo
+//
+//  Created by Mirian Santana on 16/03/23.
+//
+
+import UIKit
+import Alamofire
+
+class TableViewViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrayPizza: Pizza?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        requestPizza()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "PizzaTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+    }
+    
+    func requestPizza() {
+        AF.request("https://p3teufi0k9.execute-api.us-east-1.amazonaws.com/v1/pizza").response { response in
+            
+            let pizza = try? JSONDecoder().decode(Pizza.self, from: response.data ?? Data())
+            self.arrayPizza = pizza
+            self.tableView.reloadData()
+        }
+    }
+
+}
+extension TableViewViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrayPizza?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? PizzaTableViewCell {
+            
+            cell.setupCell(pizza: self.arrayPizza?[indexPath.row])
+            
+            return cell
+        }
+        return UITableViewCell() 
+    }
+}
+
