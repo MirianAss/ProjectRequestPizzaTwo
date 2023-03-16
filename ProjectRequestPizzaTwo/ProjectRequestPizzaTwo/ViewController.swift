3f56a0b6f8f282e7441c454d28850c46f2198aa8
Mirian Santana <miriansantana93@gmail.com>
2023-03-16 15:46:40 -0300

    Added pods with SDWebImage and Alamofire. Button on storyboard connected to viewContoller class

---
+++ ProjectRequestPizzaTwo/ProjectRequestPizzaTwo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ProjectRequestPizzaTwo
+//
+//  Created by Mirian Santana on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btStart(_ sender: Any) {
+        
+        if let screen = self.storyboard?.instantiateViewController(withIdentifier: "navigation") {
+            
+            self.present(screen, animated: true)
+        }
+    }
+}
+
